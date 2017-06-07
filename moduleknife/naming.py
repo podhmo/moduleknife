1b83750cd7215fd110549ea468645fc86dfac6fd
podhmo <ababjam61+github@gmail.com>
2017-06-07 23:32:41 +0900

    sys.path modulename

--- moduleknife/naming.py
+++ moduleknife/naming.py
@@ -1,11 +1,17 @@
 import sys
 
+sys_paths = None
 
-def modulename_of(path):
+
+def modulename_of(path, reload=False):
     """module name from filepath"""
+    global sys_paths
+    if reload or sys_paths is None:
+        sys_paths = sorted(sys.path, key=lambda x: -len(x))
+
     if not path.endswith((".py", ".pyc")):
         return path
-    for syspath in sys.path:
+    for syspath in sys_paths:
         path = path.replace(syspath, "")
     if path.endswith(".pyc"):
         path = path[:-1]
