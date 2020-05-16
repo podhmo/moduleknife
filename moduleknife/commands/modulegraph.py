7e21b504c856b6e47844e6845ff003598cf59787
podhmo <ababjam61+github@gmail.com>
2020-05-17 07:42:52 +0900

    feat: add --browse option

--- moduleknife/commands/modulegraph.py
+++ moduleknife/commands/modulegraph.py
@@ -14,10 +14,15 @@ logger = logging.getLogger(__name__)
 class Driver:
     factory = graph.Digraph
 
-    def __init__(self, filename, metadata_handler):
+    def __init__(self, filename, metadata_handler, *, browse=False):
         self.dag = self.factory()
         self.filename = filename
         self.metadata_handler = metadata_handler
+        self.browse = browse
+        if self.browse and self.filename is None:
+            import tempfile
+
+            self.filename = tempfile.mkstemp(suffix=".dot")[1]
 
     def add(self, src, dst, *, stage):
         metadata = self.metadata_handler.handle(src, dst, stage=stage)
@@ -33,6 +38,16 @@ class Driver:
                 wf.write(str(self.dag.to_dot(self.metadata_handler.emit)))
                 self.metadata_handler.finish(wf)
             print("write {}...".format(self.filename), file=sys.stderr)
+            if self.browse:
+                import shutil
+
+                if shutil.which("dot"):
+                    import subprocess
+
+                    subprocess.run(["dot", "-Tsvg", "-O", self.filename], check=True)
+                    # for mac
+                    if shutil.which("open"):
+                        subprocess.run(["open", "{}.svg".format(self.filename)])
 
     def run(self, fname, extras):
         return calling.call_file(fname, extras)
@@ -120,6 +135,7 @@ def main():
     parser.add_argument("file")
     parser.add_argument("--outfile", default=None)
     parser.add_argument("--metadata", choices=["time", "memory"], default="time")
+    parser.add_argument("--browse", action="store_true")
     parser.add_argument(
         "--driver",
         default="moduleknife.commands.modulegraph:Driver",
@@ -129,7 +145,7 @@ def main():
 
     driver_cls = import_symbol(args.driver, ns="moduleknife.commands.modulegraph")
     metadata_handler = globals().get(args.metadata + "Handler", timeHandler)()
-    driver = driver_cls(args.outfile, metadata_handler)
+    driver = driver_cls(args.outfile, metadata_handler, browse=args.browse)
 
     with capture_with_signal_handle(driver.add, teardown=driver.finish):
         driver.run(args.file, extras)
